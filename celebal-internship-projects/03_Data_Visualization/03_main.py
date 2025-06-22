129205306dd6081493a4807c60df3f51917d1ff2
RitikaChoudhary-debug <ritikachoudhary4rc@gmail.com>
2025-06-22 15:48:04 +0530

    Fixed python script

--- celebal-internship-projects/03_Data_Visualization/03_main.py
+++ celebal-internship-projects/03_Data_Visualization/03_main.py
@@ -7,7 +7,6 @@ Description: Visualizes mental health support, awareness, and patterns among tec
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
